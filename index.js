93ffd0a0d5b935758da9b9ac657f5b3c1e08c1b2
Esteban Perez <lagartosoy@gmail.com>
2023-02-27 21:34:30 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("CV interaccion");
+
+document.getElementById("estadolaboral").onclick = function(){
+    
+    document.getElementById("resultado").innerHTML= "Estoy disponible, continuo desempleado, puedes completar el formulario presentado debajo. Muchas gracias!"
+}
